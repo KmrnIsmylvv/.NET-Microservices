e954f3052ddfe287f25508118316b157595b1cff
DESKTOP-50U56HO <kamranvi@code.edu.az>
2022-06-15 07:46:16 +0400

    feat: Adding PlatformPublishedDto and mapping

--- PlatformService/Profiles/PlatformsProfile.cs
+++ PlatformService/Profiles/PlatformsProfile.cs
@@ -11,6 +11,7 @@ namespace PlatformService.Profiles
             // Source -> Target
             CreateMap<Platform, PlatformReadDto>();
             CreateMap<PlatformCreateDto, Platform>();
+            CreateMap<PlatformReadDto, PlatformPublishedDto>();
         }
     }
 }
